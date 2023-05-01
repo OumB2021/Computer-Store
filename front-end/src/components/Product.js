8398f87e1656edf70ae37de8914205d86113eebe
Oumb2021 <baarryoumar@gmail.com>
2023-05-01 02:24:27 -0400

    fake products added

--- front-end/src/components/Product.js
+++ front-end/src/components/Product.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
 import Rating from "./Rating";
+import "../css/product.css";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" />
+        <div className="">
+          <Card.Img src={product.image} variant="top" as="img" />
+        </div>
       </Link>
       <Card.Body>
         <Link
