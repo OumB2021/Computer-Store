f1a47f7f54658f5206897c1e20d53636cb808c90
Oumb2021 <baarryoumar@gmail.com>
2023-05-13 03:41:35 -0400

    user controller and authentication done

--- back-end/server.js
+++ back-end/server.js
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { authUser } from "./controllers/userController.js";
 
 dotenv.config();
 connectDB();
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Api is running...");
