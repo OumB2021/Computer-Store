6f3421d5c1b0fd4dafe32a8c9a0c8eae6546edb5
Oumb2021 <baarryoumar@gmail.com>
2023-05-14 08:12:07 -0400

    token generation and manipulation done

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -23,4 +23,23 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get User profile
+// @route   POST /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("user not found");
+  }
+});
+
 export { authUser };
--- back-end/server.js
+++ back-end/server.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
-import { authUser } from "./controllers/userController.js";
+import userRoutes from "./routes/userRoutes.js";
+
 
 dotenv.config();
 connectDB();
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
